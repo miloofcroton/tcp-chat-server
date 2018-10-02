ba1d374101ad9e5ce63a149b8ceee0785712b14c
Jack <ajackofsometrades@gmail.com>
2018-10-02 11:04:34 -0700

    constants should be all caps

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require('./lib/app');
 
-const port = 1234;
+const PORT = 1234;
 
 app.on('listening', () => {
     console.log('chat app started on port', PORT);
 });
 
-app.listen(port);
+app.listen(PORT);
