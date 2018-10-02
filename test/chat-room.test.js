2db7515ea9e2e3e0fa090723aae6ead16d4ad798
Jack <ajackofsometrades@gmail.com>
2018-10-01 22:01:32 -0700

    putting all tests on same level of hierarchy

--- test/chat-room.test.js
+++ test/chat-room.test.js
@@ -16,17 +16,24 @@ describe('chatroom tests', () => {
         chatroom.add(c3);
     });
 
-    it('assigns names', () => {
-        assert.equal(c1.username, 'user1');
-        assert.equal(c2.username, 'user2');
-        assert.equal(c3.username, 'user3');
+    describe('add(client) tests', () => {
+        
+        it('assigns names', () => {
+            assert.equal(c1.username, 'user1');
+            assert.equal(c2.username, 'user2');
+            assert.equal(c3.username, 'user3');
+        });
     });
 
-    it('gets the client you add', () => {
-        assert.ok(chatroom.getClient('user1'));
+    describe('getClient tests', () => {
+
+        it('gets the client you add', () => {
+            assert.ok(chatroom.getClient('user1'));
+        });
     });
 
     describe('rename tests', () => {
+
         it('the rename function exists', () => {
             assert.ok(chatroom.rename);
         });
