7f9808ddcdb77282e21c0b4d049d7a9d1ec3ce45
Jack <ajackofsometrades@gmail.com>
2018-10-01 17:18:25 -0700

    using map instead of set

--- test/chat-room.test.js
+++ test/chat-room.test.js
@@ -21,4 +21,10 @@ describe('chatroom tests', () => {
         assert.equal(c3.username, 'user3');
     });
 
+    it('assigns names', () => {
+        assert.equal(c1.username, 'user1');
+        assert.equal(c2.username, 'user2');
+        assert.equal(c3.username, 'user3');
+    });
+
 });
