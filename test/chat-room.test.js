41887707f854faded05caa662b8fae3dd50cc0e8
Jack <ajackofsometrades@gmail.com>
2018-10-01 17:26:12 -0700

    failing test for getting user

--- test/chat-room.test.js
+++ test/chat-room.test.js
@@ -21,10 +21,9 @@ describe('chatroom tests', () => {
         assert.equal(c3.username, 'user3');
     });
 
-    it('assigns names', () => {
-        assert.equal(c1.username, 'user1');
-        assert.equal(c2.username, 'user2');
-        assert.equal(c3.username, 'user3');
+    it('gets the client you add', () => {
+        let chosenClient = getClient('user1');
+        assert.ok(chosenClient);
     });
 
 });
